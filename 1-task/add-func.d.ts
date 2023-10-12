feb22f1ed3c82d30761c443e6f2073b6ddabc4bd
ryzhayaa <bannova030800@gmail.com>
2023-10-12 22:55:31 +0500

    1st quest

--- 1-task/add-func.d.ts
+++ 1-task/add-func.d.ts
@@ -1,1 +1,8 @@
 export type Add = (x: number, y: number) => number;
+
+export const add: Add = (x, y) => {
+if (typeof x !== 'number' || typeof y !== 'number' || isNaN(x) || isNaN(y) || !isFinite(x) || !isFinite(y)) {
+throw new Error('WrongType');
+}
+return x + y;
+};
